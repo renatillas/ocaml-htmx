a01b4804c0952e935c43f843fdb9e4f77bf21f16
renatillas <renata.amutio@gmail.com>
2023-10-30 01:55:35 +0100

    feat: edit UI improvement

--- lib/infrastructure/templates/edit.ml
+++ lib/infrastructure/templates/edit.ml
@@ -14,72 +14,81 @@ let render
         [ Unsafe.data (Dream.csrf_tag request)
         ; fieldset
             ~legend:(legend [ txt "Contact values" ])
-            [ p
-                [ label ~a:[ a_label_for "email" ] [ txt "Email" ]
-                ; input
-                    ~a:
-                      [ a_name "email"
-                      ; a_id "email"
-                      ; a_input_type `Email
-                      ; a_placeholder "Email"
-                      ; a_value contact.email
-                      ; Unsafe.string_attrib
-                          "hx-get"
-                          (Printf.sprintf "/contacts/%i/email" contact_id)
-                      ; Unsafe.string_attrib "hx-target" "next .error"
-                      ; Unsafe.string_attrib "hx-trigger" "change, keyup delay:200ms"
-                      ]
-                    ()
-                ; span
-                    ~a:[ a_class [ "error"; "color"; "bad" ] ]
-                    [ txt Option.(value ~default:"" (errors >>= fun err -> err.email)) ]
+            [ div
+                ~a:[ a_class [ "table"; "rows" ] ]
+                [ p
+                    [ label ~a:[ a_label_for "email" ] [ txt "Email" ]
+                    ; input
+                        ~a:
+                          [ a_name "email"
+                          ; a_id "email"
+                          ; a_input_type `Email
+                          ; a_placeholder "Email"
+                          ; a_value contact.email
+                          ; Unsafe.string_attrib
+                              "hx-get"
+                              (Printf.sprintf "/contacts/%i/email" contact_id)
+                          ; Unsafe.string_attrib "hx-target" "next .error"
+                          ; Unsafe.string_attrib
+                              "hx-trigger"
+                              "change, keyup delay:200ms changed"
+                          ]
+                        ()
+                    ; span
+                        ~a:[ a_class [ "error"; "color"; "bad" ] ]
+                        [ txt Option.(value ~default:"" (errors >>= fun err -> err.email))
+                        ]
+                    ]
+                ; p
+                    [ label ~a:[ a_label_for "first_name" ] [ txt "First Name" ]
+                    ; input
+                        ~a:
+                          [ a_name "first_name"
+                          ; a_id "first_name"
+                          ; a_input_type `Text
+                          ; a_placeholder "First name"
+                          ; a_value contact.first
+                          ]
+                        ()
+                    ; span
+                        ~a:[ a_class [ "color"; "bad" ] ]
+                        [ txt Option.(value ~default:"" (errors >>= fun err -> err.first))
+                        ]
+                    ]
+                ; p
+                    [ label ~a:[ a_label_for "last_name" ] [ txt "Last Name" ]
+                    ; input
+                        ~a:
+                          [ a_name "last_name"
+                          ; a_id "last_name"
+                          ; a_input_type `Text
+                          ; a_placeholder "Last Name"
+                          ; a_value contact.last
+                          ]
+                        ()
+                    ; span
+                        ~a:[ a_class [ "color"; "bad" ] ]
+                        [ txt Option.(value ~default:"" (errors >>= fun err -> err.last))
+                        ]
+                    ]
+                ; p
+                    [ label ~a:[ a_label_for "phone" ] [ txt "Phone" ]
+                    ; input
+                        ~a:
+                          [ a_name "phone"
+                          ; a_id "phone"
+                          ; a_input_type `Text
+                          ; a_placeholder "Phone"
+                          ; a_value contact.phone
+                          ]
+                        ()
+                    ; span
+                        ~a:[ a_class [ "color"; "bad" ] ]
+                        [ txt Option.(value ~default:"" (errors >>= fun err -> err.phone))
+                        ]
+                    ]
+                ; button [ txt "Save" ]
                 ]
-            ; p
-                [ label ~a:[ a_label_for "first_name" ] [ txt "First Name" ]
-                ; input
-                    ~a:
-                      [ a_name "first_name"
-                      ; a_id "first_name"
-                      ; a_input_type `Text
-                      ; a_placeholder "First name"
-                      ; a_value contact.first
-                      ]
-                    ()
-                ; span
-                    ~a:[ a_class [ "color"; "bad" ] ]
-                    [ txt Option.(value ~default:"" (errors >>= fun err -> err.first)) ]
-                ]
-            ; p
-                [ label ~a:[ a_label_for "last_name" ] [ txt "Last Name" ]
-                ; input
-                    ~a:
-                      [ a_name "last_name"
-                      ; a_id "last_name"
-                      ; a_input_type `Text
-                      ; a_placeholder "Last Name"
-                      ; a_value contact.last
-                      ]
-                    ()
-                ; span
-                    ~a:[ a_class [ "color"; "bad" ] ]
-                    [ txt Option.(value ~default:"" (errors >>= fun err -> err.last)) ]
-                ]
-            ; p
-                [ label ~a:[ a_label_for "phone" ] [ txt "Phone" ]
-                ; input
-                    ~a:
-                      [ a_name "phone"
-                      ; a_id "phone"
-                      ; a_input_type `Text
-                      ; a_placeholder "Phone"
-                      ; a_value contact.phone
-                      ]
-                    ()
-                ; span
-                    ~a:[ a_class [ "color"; "bad" ] ]
-                    [ txt Option.(value ~default:"" (errors >>= fun err -> err.phone)) ]
-                ]
-            ; button [ txt "Save" ]
             ]
         ]
     ; button
